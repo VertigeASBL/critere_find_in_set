63c5c343b2c257651e4a94449bce8b4ed86e8db0
Debondt Didier <p@henix.be>
2014-10-23 12:26:00 +0200

    Meilleur description

--- lang/paquet-findinset_fr.php
+++ lang/paquet-findinset_fr.php
@@ -6,9 +6,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// F
-	'findinset_description' => '',
+	'findinset_description' => "Permet de filtrer une boucle SPIP via une valeur d'un champ SQL à séparateur virgule (valeur1,valeur2,valeur3,...).",
 	'findinset_nom' => 'Critère find_in_set',
-	'findinset_slogan' => 'Utiliser le find_in_set de mysql dans les boucles SPIP',
+	'findinset_slogan' => 'Utiliser le find_in_set de mysql dans les boucles SPIP.',
 );
 
 ?>
